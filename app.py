17ab4493585f5095d03ac4ffd7e746e6cb3b0f5c
bitbrain <bitbrainresearch@gmail.com>
2025-07-15 00:18:17 -0400

    added application root

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Configure for subdirectory deployment
 app.config['PREFERRED_URL_SCHEME'] = 'https'
+app.config['APPLICATION_ROOT'] = '/lrtc'
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
